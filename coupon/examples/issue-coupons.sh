3acb74657a36eba8876fec464b0e59a021da126b
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2021-10-20 14:18:15 +0200

    Add markdown documentaion of [POST] /coupon/v1/coupons

---
+++ coupon/examples/issue-coupons.sh
@@ -0,0 +1,11 @@
+ACCESS_TOKEN= your access token
+PRESET= unique preset slug
+QUANTITY= the quantity of coupons
+
+COUPONS_JSON=$(curl "https://public-api.wordpress.com/wpcom/v2/jetpack-partner/coupon/v1/coupons" \
+  --silent \
+  --header "Authorization: Bearer $ACCESS_TOKEN" \
+  --header "Content-Type: application/json" \
+  --data "{\"preset\":\"$PRESET\",\"quantity\":\"$QUANTITY\"}" \
+  -X POST)
+echo $COUPONS_JSON
